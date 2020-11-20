307f66d9645d9f5dc0f957dd8d5786f78ead7cfb
McJoe21 <39857764+McJoe21@users.noreply.github.com>
2020-11-20 21:56:58 +0000

    Changed game data storage

--- index.js
+++ index.js
@@ -5,9 +5,7 @@ var u = document.getElementById('upgrade');
 var info = document.getElementById('info')
 var main = document.getElementById('main')
 
-var money = 0;
-var count = 0;
-var level = 1;
+var ud = [{"m":0,"c":0,"l":1}]
 var shop = [{"name":"click","base":1,"b_added":1,"price":25,"p_added":10}]
 var max = 20;
 var added = 25;
@@ -16,14 +14,14 @@ bar.max = max;
 u.innerHTML = "Upgrade Click (💰"+shop[0]["price"]+")"
 
 function click() {
-  money += shop[0]["base"];
-  count += shop[0]["base"];
-  h.innerHTML = "💰 "+money+ " ⭐ "+level;
-  bar.value = count;
+  ud[0]["m"] += shop[0]["base"];
+  ud[0]["c"] += shop[0]["base"];
+  h.innerHTML = "💰 "+ud[0]["m"]+" ⭐ "+ud[0]["l"];
+  bar.value = ud[0]["c"];
   
-  if (count > max-1){
-    count = 0;
-    level += 1
+  if (ud[0]["c"] > max-1){
+    ud[0]["c"] = 0;
+    ud[0]["l"] += 1
     bar.value = 0;
     max += added;
     bar.max = max;
@@ -32,17 +30,17 @@ function click() {
 
 function buy(item){
   if (item == 0){
-    if (money > shop[item]["price"]-1){
+    if (ud[0]["m"] > shop[item]["price"]-1){
       shop[item]["base"] += shop[item]["b_added"]
-      money -= shop[item]["price"]
+      ud[0]["m"] -= shop[item]["price"]
       shop[item]["price"] += shop[item]["p_added"]
-      h.innerHTML = "💰 "+money+ " ⭐ "+level;
+      h.innerHTML = "💰 "+ud[0]["m"]+ " ⭐ "+ud[0]["l"];
       u.innerHTML = "Upgrade Click (💰"+shop[0]["price"]+")"
     }
   }
 }
 
-function toggleDiv(){
+function playgame(){
   main.style.display = 'block'
   info.style.display = 'none'
 }
